f09b5b57b2f0cfd6889a5a38324a7310f3ef8ca8
Sergey Vanyushkin <pi3c@yandex.ru>
2024-01-26 09:52:35 +0000

    just for sync

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,21 +1,30 @@
 import pytest
 from httpx import AsyncClient
 
-
 url = "http://localhost:8000/api/v1/menus"
 
 
-@pytest.mark.asyncio
-async def test_read_menus(app):
-    async with AsyncClient(app=app, base_url=url) as ac:
-        response = await ac.get("/")
-    assert response.status_code == 200
-    assert response.json() == []
+class TestCrud:
+    @pytest.mark.asyncio
+    async def test_read_menus(self, app):
+        """тест пустой бд"""
+        async with AsyncClient(app=app, base_url=url) as ac:
+            response = await ac.get("/")
+        assert response.status_code == 200
+        assert response.json() == []
+
+    @pytest.mark.asyncio
+    async def test_write_menu(self, app):
+        """"""
+        async with AsyncClient(app=app, base_url=url) as ac:
+            response = await ac.post("/", json={"title": "menu 1", "description": None})
+        assert response.status_code == 201
+        assert response.json()["title"] == "menu 1"
+        assert response.json()["description"] == None
 
 
-@pytest.mark.asyncio
-async def test_write_menu(app):
-    async with AsyncClient(app=app, base_url=url) as ac:
-        response = await ac.post("/", json={"title": "ddd", "description": "hh"})
-    assert response.status_code == 201
-    assert response.json()["title"] == "ddd"
+class TestСontinuity:
+    @pytest.mark.asyncio
+    async def test_postman_continuity(self, app):
+        async with AsyncClient(app=app, base_url=url) as ac:
+            pass
